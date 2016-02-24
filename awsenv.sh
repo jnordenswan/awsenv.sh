78b5e7072bc8cca46241a30eff473cc2f18bebac
jnordenswan <jnordenswan@gmail.com>
2016-02-24 15:35:11 +0100

    wip

--- awsenv.sh
+++ awsenv.sh
@@ -15,19 +15,19 @@ if [ -s $CONFFILE ]
 	read AWS_DEFAULT_REGION
         printf "Enter AWS acces key ID: "
         read AWS_ACCESS_KEY_ID
-        printf "Enter AWS secret access key: (will not echo)"
+        printf "Enter AWS secret access key: (will not echo) "
         read -s AWS_SECRET_ACCESS_KEY
+	echo
 	printf "Additional PyPI packages to include, space separated: "
 	read PYPI_PACKAGES
-	echo
 	echo "PS1_PREFIX=$PS1_PREFIX" > $CONFFILE
 	echo "AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION" >> $CONFFILE
         echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> $CONFFILE
         echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> $CONFFILE
-	echo "PYPI_PACKAGES=$PYPI_PACKAGES" >> $CONFFILE
+	echo "PYPI_PACKAGES=\"$PYPI_PACKAGES\"" >> $CONFFILE
 fi
 
-INITSTR="$TEMPDIR/virtualenv/virtualenv.py $TEMPDIR/awsenv --prompt=$PS1_PREFIX && \
+INITSTR="$TEMPDIR/virtualenv/virtualenv.py $TEMPDIR/awsenv --prompt=\"$PS1_PREFIX \" && \
          source $TEMPDIR/awsenv/bin/activate && \
          pip install awscli $PYPI_PACKAGES && \
          alias deactivate=exit && \
@@ -36,6 +36,7 @@ INITSTR="$TEMPDIR/virtualenv/virtualenv.py $TEMPDIR/awsenv --prompt=$PS1_PREFIX 
          export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID && \
          export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY && \
          if [ -s ~/.bashrc ]; then source ~/.bashrc; fi && \
+	 if [ $SHELL == '/bin/bash' ]; then complete -C '$TEMPDIR/awsenv/bin/aws_completer' aws; fi && \
          echo"
 
 git clone --depth 1 git@github.com:pypa/virtualenv.git $TEMPDIR/virtualenv
